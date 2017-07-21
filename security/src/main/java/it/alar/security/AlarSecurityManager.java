32b61196f8671fe08c091a49102cad7da1aa15db
Alessandro Arici <a.arici@techgap.it>
2017-07-21 16:00:11 +0200

    Added License

--- security/src/main/java/it/alar/security/AlarSecurityManager.java
+++ security/src/main/java/it/alar/security/AlarSecurityManager.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2017 the original author or authors.
+ */
+
 package it.alar.security;
 
 import javax.annotation.PostConstruct;
@@ -6,6 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * This file is part of Alar-Security
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *	@author Alessandro Arici
+ *  @since 1.0
+ */
+
 
 @Component
 public class AlarSecurityManager {
