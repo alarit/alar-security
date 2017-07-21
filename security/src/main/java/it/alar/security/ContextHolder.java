418ae2a19f11353f66074c1974662a9cd6b31be6
Alessandro Arici <a.arici@techgap.it>
2017-07-21 16:05:54 +0200

    Updated License

--- security/src/main/java/it/alar/security/ContextHolder.java
+++ security/src/main/java/it/alar/security/ContextHolder.java
@@ -1,25 +1,25 @@
 /*
  * Copyright 2017 the original author or authors.
- */
-
-package it.alar.security;
-
-/**
+ * 
  * This file is part of Alar-Security
  * 
- * This program is free software: you can redistribute it and/or modify
+ * Alar-security is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Alar-security is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser Public License for more details.
  *
  * You should have received a copy of the GNU Lesser Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ */
+
+package it.alar.security;
+
+/**
  *	@author Alessandro Arici
  *  @since 1.0
  */
